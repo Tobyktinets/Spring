fd60e0f053daa5a6f21274b20e68f07b42c5b703
Mukhtar <kompnakuhne@mail.ru>
2019-07-12 12:51:28 +0600

    add final

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -21,9 +21,9 @@ public class Application {
 
             System.out.println("Let's inspect the beans provided by Spring Boot:");
 
-            String[] beanNames = ctx.getBeanDefinitionNames();
+            final String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
+            for (final String beanName : beanNames) {
                 System.out.println(beanName);
             }
 
